cea43716d97d87b2b2e6282d8bfd7dec3ee92580
Valentina Lugovaia <vaal.it@yandex.ru>
2022-10-07 18:45:05 +0300

    add other routes

--- controllers/cards.js
+++ controllers/cards.js
@@ -8,16 +8,26 @@ module.exports.getCards = (req, res) => {
 
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
-  const owner = req.user._id;
-  const likes = [];
 
-  Card.create({ name, link, owner, likes })
+  Card.create({ name, link, owner: req.user._id })
     .then(card => res.send({ data: card }))
     .catch(err => res.status(500).send({ message: 'Произошла ошибка' }));
 };
 
 module.exports.deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
+    .then(card => res.send({ data: card }))
+    .catch(err => res.status(500).send({ message: 'Произошла ошибка' }));
+};
+
+module.exports.likeCard = (req, res) => {
+  Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
+    .then(card => res.send({ data: card }))
+    .catch(err => res.status(500).send({ message: 'Произошла ошибка' }));
+};
+
+module.exports.dislikeCard = (req, res) => {
+  Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
     .then(card => res.send({ data: card }))
     .catch(err => res.status(500).send({ message: 'Произошла ошибка' }));
 };
