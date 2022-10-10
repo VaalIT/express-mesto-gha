96eb9feead6f2452d252c838824e06ea9f105bad
Valentina Lugovaia <vaal.it@yandex.ru>
2022-10-10 18:11:32 +0300

    fix errors

--- controllers/cards.js
+++ controllers/cards.js
@@ -23,51 +23,51 @@ module.exports.createCard = (req, res) => {
 
 module.exports.deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
-    .orFail(() => new Error('Карточка не найдена'))
+    .orFail(() => {
+      throw new Error('Пользователь не найден');
+    })
     .then((card) => res.send({ data: card }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         res.status(NOT_FOUND).send({ message: 'Карточка с указанным id не найдена' });
-      } else if (err.name === 'NotFoundError') {
-        res.status(NOT_FOUND).send({ message: 'Карточка с указанным id не найдена' });
       } else if (err.name === 'CastError') {
         res.status(BAD_REQUEST).send({ message: 'Передан некорректный id карточки' });
       } else {
-        res.status(INTERNAL_SERVER_ERROR).send({ message: 'Произошла внутренняя ошибка сервера' });
+        res.status(NOT_FOUND).send({ message: 'Карточка с указанным id не найдена' });
       }
     });
 };
 
 module.exports.likeCard = (req, res) => {
   Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
-    .orFail(() => new Error('Карточка не найдена'))
+    .orFail(() => {
+      throw new Error('Пользователь не найден');
+    })
     .then((card) => res.send({ data: card }))
     .catch((err) => {
-      if (err.message === 'Not Found') {
+      if (err.name === 'ValidationError') {
         res.status(NOT_FOUND).send({ message: 'Карточка с указанным id не найдена' });
-      } else if (err.name === 'NotFoundError') {
-        res.status(NOT_FOUND).send({ message: 'Передан несуществующий id карточки' });
       } else if (err.name === 'CastError') {
         res.status(BAD_REQUEST).send({ message: 'Переданы некорректные данные для постановки лайка' });
       } else {
-        res.status(INTERNAL_SERVER_ERROR).send({ message: 'Произошла внутренняя ошибка сервера' });
+        res.status(NOT_FOUND).send({ message: 'Передан несуществующий id карточки' });
       }
     });
 };
 
 module.exports.dislikeCard = (req, res) => {
   Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
-    .orFail(() => new Error('Карточка не найдена'))
+    .orFail(() => {
+      throw new Error('Пользователь не найден');
+    })
     .then((card) => res.send({ data: card }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         res.status(NOT_FOUND).send({ message: 'Карточка с указанным id не найдена' });
       } else if (err.name === 'CastError') {
         res.status(BAD_REQUEST).send({ message: 'Переданы некорректные данные для снятия лайка' });
-      } else if (err.name === 'NotFoundError') {
-        res.status(NOT_FOUND).send({ message: 'Передан несуществующий id карточки' });
       } else {
-        res.status(INTERNAL_SERVER_ERROR).send({ message: 'Произошла внутренняя ошибка сервера' });
+        res.status(NOT_FOUND).send({ message: 'Передан несуществующий id карточки' });
       }
     });
 };
