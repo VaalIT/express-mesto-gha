35b0f998d5391728494064472bd2496313f366e5
Valentina Lugovaia <vaal.it@yandex.ru>
2022-10-10 16:43:00 +0300

    fix custom error

--- controllers/users.js
+++ controllers/users.js
@@ -12,10 +12,10 @@ module.exports.getUserById = (req, res) => {
     .orFail(() => new Error('Пользователь не найден'))
     .then((user) => res.send({ data: user }))
     .catch((err) => {
-      if (err.name === 'NotFoundError') {
-        res.status(NOT_FOUND).send({ message: 'Пользователь по указанному id не найден' });
-      } else if (err.name === 'CastError') {
+      if (err.name === 'CastError') {
         res.status(BAD_REQUEST).send({ message: 'Передан некорректный id пользователя' });
+      } else if (err.name === 'NotFoundError') {
+        res.status(NOT_FOUND).send({ message: 'Пользователь по указанному id не найден' });
       } else {
         res.status(INTERNAL_SERVER_ERROR).send({ message: 'Произошла внутренняя ошибка сервера' });
       }
