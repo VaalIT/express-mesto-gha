e0835fadb184830638123a66df14a5c78018f51b
Valentina Lugovaia <vaal.it@yandex.ru>
2022-10-21 01:10:32 +0300

    rework user model, users controller, create login controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,28 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
-const { BAD_REQUEST, NOT_FOUND, INTERNAL_SERVER_ERROR } = require('../errors');
+const {
+  BAD_REQUEST, UNAUTHORIZED, NOT_FOUND, INTERNAL_SERVER_ERROR,
+} = require('../errors');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
+
+module.exports.login = (req, res) => {
+  const { email, password } = req.body;
+
+  return User.findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
+
+      res
+        .cookie('jwt', token, {
+          maxAge: 3600000 * 24 * 7,
+          httpOnly: true,
+          sameSite: true,
+        });
+    })
+    .catch(() => res.status(UNAUTHORIZED).send({ message: 'Отказано в доступе' }));
+};
 
 module.exports.getAllUsers = (req, res) => {
   User.find({})
@@ -25,9 +48,15 @@ module.exports.getUserById = (req, res) => {
 };
 
 module.exports.createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
+  const {
+    name, about, avatar, email, password,
+  } = req.body;
 
-  User.create({ name, about, avatar })
+  bcrypt
+    .hash(password, 10)
+    .then((hash) => User.create({
+      name, about, avatar, email, password: hash,
+    }))
     .then((user) => res.send({ data: user }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
--- errors.js
+++ errors.js
@@ -1,11 +1,14 @@
 const BAD_REQUEST = 400;
 
+const UNAUTHORIZED = 401;
+
 const NOT_FOUND = 404;
 
 const INTERNAL_SERVER_ERROR = 500;
 
 module.exports = {
   BAD_REQUEST,
+  UNAUTHORIZED,
   NOT_FOUND,
   INTERNAL_SERVER_ERROR,
 };
