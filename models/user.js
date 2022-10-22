5fa3cf6a3b2e8da11bfcf344edbb78cdacee28ed
Valentina Lugovaia <vaal.it@yandex.ru>
2022-10-22 16:56:45 +0300

    fix user model

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
-const { UNAUTHORIZED } = require('../utils/errors');
+const Unauthorized = require('../utils/Unauthorized');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -40,13 +40,13 @@ userSchema.statics.findUserByCredentials = function findUserByCredentials(email,
     .select('+password')
     .then((user) => {
       if (!user) {
-        return Promise.reject(new UNAUTHORIZED('Неправильные почта или пароль'));
+        return Promise.reject(new Unauthorized('Неправильные почта или пароль'));
       }
 
       return bcrypt.compare(password, user.password)
         .then((matched) => {
           if (!matched) {
-            return Promise.reject(new UNAUTHORIZED('Неправильные почта или пароль'));
+            return Promise.reject(new Unauthorized('Неправильные почта или пароль'));
           }
 
           return user;
